56a8980964853e5bce9c7e734d3d02891f79485c
EuReinoso <lucas.reinoso.br@gmail.com>
2023-06-04 19:42:52 -0300

    Ajustes Finais e Executavel

---
+++ ClienteCadastroWPF/Forms/Main/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using ClienteCadastroWPF.Forms.Cliente;
+using ClienteCadastroWPF.Forms.Integracao;
+
+namespace ClienteCadastroWPF.Forms.Main
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void btnClientes_Click(object sender, RoutedEventArgs e)
+        {
+            ClienteMenuWindow window = new ClienteMenuWindow();
+            window.Show();
+        }
+
+        private void btnIntegracao_Click(object sender, RoutedEventArgs e)
+        {
+            IntegracaoWindow window = new IntegracaoWindow();
+            window.Show();
+
+            this.Close();
+        }
+    }
+}
